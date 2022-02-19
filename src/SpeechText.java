697e138ce617fe43b3b24094ba2a2706e738ac1c
Juho <juho-kim@outlook.com>
2022-02-19 18:23:23 -0500

    Allow workout routine to be played as audio

--- src/SpeechText.java
+++
@@ -1,9 +0,0 @@
-public class SpeechText {
-    /**
-     * System will listen say the message provided in message
-     */
-    static String listen() {
-        String message;
-        return "";
-    }
-}
