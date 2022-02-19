f2bdc82484e3a67ddcc9cb6a2fae5a4188709d33
Juho <juho-kim@outlook.com>
2022-02-19 17:17:30 -0500

    make compatible with Java 11

--- src/Workout.java
+++ src/Workout.java
@@ -8,19 +8,44 @@ public class Workout {
     private String name;
     private int repCount;
     private int setCount;
-    private int duration;
+    private double duration;
 
-    public Workout(String name, int setCount, int repCount) {
+    public Workout(String name, int setCount, int repCount, double duration) {
         this.name = name;
         this.setCount = setCount;
         this.repCount = repCount;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSetCount() {
+        return setCount;
     }
 
     public void setSetCount(int setCount) {
         this.setCount = setCount;
     }
 
+    public int getRepCount() {
+        return repCount;
+    }
+
     public void setRepCount(int repCount) {
         this.repCount = repCount;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 }
--- src/WorkoutManager.java
+++ src/WorkoutManager.java
@@ -19,32 +19,32 @@ public class WorkoutManager {
         createArmWorkout();
     }
 
-    public Workout createWorkout(String name, int set, int rep) {
-        return new Workout(name, set, rep);
+    public Workout createWorkout(String name, int setCount, int repCount, double duration) {
+        return new Workout(name, setCount, repCount, duration);
     }
 
     public void createLegWorkout() {
-        Workout leg1 = createWorkout("Squates", 10, 3);
-        Workout leg2 = createWorkout("Lunges", 10, 3);
-        Workout leg3 = createWorkout("Knee ups", 10, 3);
+        Workout leg1 = createWorkout("Squats", 10, 3, 10);
+        Workout leg2 = createWorkout("Lunges", 10, 3, 10);
+        Workout leg3 = createWorkout("Knee ups", 10, 3, 10);
         legsWorkouts.add(leg1);
         legsWorkouts.add(leg2);
         legsWorkouts.add(leg3);
     }
 
     public void createCoreWorkout() {
-        Workout core1 = createWorkout("Sit ups", 10, 3);
-        Workout core2 = createWorkout("Crunches", 10, 3);
-        Workout core3 = createWorkout("Russian Twist", 10, 3);
+        Workout core1 = createWorkout("Sit ups", 10, 3, 10);
+        Workout core2 = createWorkout("Crunches", 10, 3, 10);
+        Workout core3 = createWorkout("Russian Twist", 10, 3, 10);
         coreWorkouts.add(core1);
         coreWorkouts.add(core2);
         coreWorkouts.add(core3);
     }
 
     public void createArmWorkout() {
-        Workout arm1 = createWorkout("Push Up", 10, 3);
-        Workout arm2 = createWorkout("Arm Dips", 10, 3);
-        Workout arm3 = createWorkout("Bicep Curls with weights", 10, 3);
+        Workout arm1 = createWorkout("Push Up", 10, 3, 10);
+        Workout arm2 = createWorkout("Arm Dips", 10, 3, 10);
+        Workout arm3 = createWorkout("Bicep Curls with weights", 10, 3, 10);
         armWorkouts.add(arm1);
         armWorkouts.add(arm2);
         armWorkouts.add(arm3);
@@ -53,11 +53,20 @@ public class WorkoutManager {
     public void createRoutine(String mode) {
         WorkoutRoutine new_routine;
         switch (mode) {
-            case "empty" -> new_routine = new WorkoutRoutine();
-            case "legs" -> new_routine = new WorkoutRoutine(legsWorkouts);
-            case "core" -> new_routine = new WorkoutRoutine(coreWorkouts);
-            case "arms" -> new_routine = new WorkoutRoutine(armWorkouts);
-            default -> throw new IllegalStateException("Unexpected value: " + mode);
+            case "empty":
+                new_routine = new WorkoutRoutine();
+                break;
+            case "legs":
+                new_routine = new WorkoutRoutine(legsWorkouts);
+                break;
+            case "core":
+                new_routine = new WorkoutRoutine(coreWorkouts);
+                break;
+            case "arms":
+                new_routine = new WorkoutRoutine(armWorkouts);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + mode);
         }
         this.workoutRoutines.add(new_routine);
     }
