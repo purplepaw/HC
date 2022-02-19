e08b7608151c69737f56af142bd8c0cca0717eb5
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2022-02-19 15:11:12 -0500

    Added temporary implementation of TextSpeech for development (will print instead of audibly say)

---
+++ src/TextSpeech.java
@@ -0,0 +1,10 @@
+public class TextSpeech {
+
+    /**
+     * System will audibly say the message provided in message
+     * @param message string that is to be said by program
+     */
+    static void say(String message) {
+        System.out.println("System Says: " + message); // temporary implementation
+    }
+}
